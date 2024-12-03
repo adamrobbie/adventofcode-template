548d56d4d76d187b3aa6a46b41b32ee3db6952b3
Adam Robbie <adam.robbie@gmail.com>
2024-12-03 11:14:31 -0500

    day 3 kinda beat me up, but still fun

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'byebug'
-
+gem 'solargraph'
 # Support libraries
 gem 'algorithms'
 
--- day-03.rb
+++ day-03.rb
@@ -2,15 +2,54 @@ require_relative 'common'
 
 class Day3 < AdventDay
   def first_part
+    sum_multiplications(input)
   end
 
   def second_part
+    sum_multiplications_with_condition(input)
   end
 
   private
 
   def convert_data(data)
     super
+  end
+
+  def sum_multiplications_with_condition(data)
+    # Regex to capture mul(a,b), do(), and don't() instructions
+    result = 0
+    enabled = true
+    data.each do |line|
+    instructions = line.scan(/mul\(\d+,\d+\)|do\(\)|don't\(\)/)
+
+
+
+    instructions.each do |instruction|
+      case instruction
+      when "do()"
+        enabled = true
+      when "don't()"
+        enabled = false
+      else
+        # Process mul(a,b) instructions only if enabled
+        result += multiplication_result(instruction) if enabled
+      end
+    end
+
+    result
+    end
+    result
+  end
+
+  def multiplication_result(line)
+    # Regex to capture mul(a,b) instructions
+    line.scan(/mul\((\d+),(\d+)\)/).map { |x, y| x.to_i * y.to_i }.sum
+  end
+
+  def sum_multiplications(data)
+    data.sum do |line|
+      multiplication_result(line)
+    end
   end
 end
 
