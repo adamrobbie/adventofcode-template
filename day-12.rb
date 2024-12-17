5f8c60f574a3f7ee1737683107b2dd8ddb0dd344
Adam Robbie <adam.robbie@gmail.com>
2024-12-17 15:13:38 -0500

    clawgame was fun man

--- day-12.rb
+++ day-12.rb
@@ -1,10 +1,142 @@
+# garden_map.rb
+class GardenMap
+  def initialize(data)
+    @data = data.map { |row| row.chars }
+    @height = data.size
+    @width = data[0].size
+    @visited = Array.new(@height) { Array.new(@width, false) }
+  end
+
+  def total_fencing_price
+    reset_visited
+    total = 0
+
+    @height.times do |i|
+      @width.times do |j|
+        next if @visited[i][j]
+        total += process_region(i, j)
+      end
+    end
+
+    total
+  end
+
+  def total_fencing_price_with_sides
+    reset_visited
+    total = 0
+
+    @height.times do |i|
+      @width.times do |j|
+        next if @visited[i][j]
+        total += process_region_with_sides(i, j)
+      end
+    end
+
+    total
+  end
+
+  private
+
+  def reset_visited
+    @visited.each { |row| row.fill(false) }
+  end
+
+  def process_region(start_i, start_j)
+    area = 0
+    perimeter = 0
+    plant = @data[start_i][start_j]
+    stack = [[start_i, start_j]]
+
+    while !stack.empty?
+      i, j = stack.pop
+      next if @visited[i][j]
+
+      @visited[i][j] = true
+      area += 1
+
+      [[0, 1], [1, 0], [0, -1], [-1, 0]].each do |(di, dj)|
+        ni, nj = i + di, j + dj
+
+        if out_of_bounds?(ni, nj) || @data[ni][nj] != plant
+          perimeter += 1
+        elsif !@visited[ni][nj]
+          stack.push([ni, nj])
+        end
+      end
+    end
+
+    area * perimeter
+  end
+
+  def process_region_with_sides(start_i, start_j)
+    area = 0
+    plant = @data[start_i][start_j]
+    stack = [[start_i, start_j]]
+
+    # Track border plots by direction
+    top_borders = {}    # by row
+    bottom_borders = {} # by row
+    left_borders = {}   # by col
+    right_borders = {}  # by col
+
+    while !stack.empty?
+      i, j = stack.pop
+      next if @visited[i][j]
+
+      @visited[i][j] = true
+      area += 1
+
+      # Check all four directions
+      check_neighbor(i, j, -1, 0, plant, top_borders, i, j)    # top
+      check_neighbor(i, j, 1, 0, plant, bottom_borders, i, j)  # bottom
+      check_neighbor(i, j, 0, -1, plant, left_borders, j, i)   # left
+      check_neighbor(i, j, 0, 1, plant, right_borders, j, i)   # right
+
+      [[0, 1], [1, 0], [0, -1], [-1, 0]].each do |(di, dj)|
+        ni, nj = i + di, j + dj
+        next if out_of_bounds?(ni, nj) || @visited[ni][nj] || @data[ni][nj] != plant
+        stack.push([ni, nj])
+      end
+    end
+
+    sides = count_sides(top_borders) + count_sides(bottom_borders) +
+           count_sides(left_borders) + count_sides(right_borders)
+
+    area * sides
+  end
+
+  def check_neighbor(i, j, di, dj, plant, borders, key, val)
+    ni, nj = i + di, j + dj
+    if out_of_bounds?(ni, nj) || @data[ni][nj] != plant
+      borders[key] ||= []
+      borders[key] << val
+    end
+  end
+
+  def count_sides(borders)
+    borders.values.sum do |coords|
+      # Sort coordinates and remove consecutive numbers
+      coords.sort.chunk_while { |a, b| b - a == 1 }.count
+    end
+  end
+
+  def out_of_bounds?(i, j)
+    i < 0 || i >= @height || j < 0 || j >= @width
+  end
+end
+
+# day12.rb
 require_relative 'common'
 
 class Day12 < AdventDay
   def first_part
+    garden = GardenMap.new(input)
+    garden.total_fencing_price
   end
 
   def second_part
+    garden = GardenMap.new(input)
+    garden.total_fencing_price_with_sides
   end
 
   private
--- day-13.rb
+++ day-13.rb
@@ -1,7 +1,77 @@
+# claw_game.rb
+class ClawGame
+  Machine = Struct.new(:button_a_x, :button_a_y, :button_b_x, :button_b_y, :prize_x, :prize_y)
+
+  def initialize(input_data)
+    @machines = parse_machines(input_data)
+  end
+
+  def minimum_tokens_needed
+    @machines.sum do |machine|
+      solve_machine(machine) || 0
+    end
+  end
+
+  private
+
+  def parse_machines(data)
+    data.join("\n").split("\n\n").map do |paragraph|
+      lines = paragraph.split("\n")
+
+      # Extract button A coordinates
+      button_a = lines[0].scan(/X\+(\d+), Y\+(\d+)/).first
+      ax = button_a[0].to_i
+      ay = button_a[1].to_i
+
+      # Extract button B coordinates
+      button_b = lines[1].scan(/X\+(\d+), Y\+(\d+)/).first
+      bx = button_b[0].to_i
+      by = button_b[1].to_i
+
+      # Extract prize coordinates
+      prize = lines[2].scan(/X=(\d+), Y=(\d+)/).first
+      px = prize[0].to_i
+      py = prize[1].to_i
+
+      Machine.new(ax, ay, bx, by, px, py)
+    end
+  end
+
+  def solve_machine(machine)
+    # Using linear algebra to solve the system of equations:
+    # ax * A + bx * B = px  (equation 1)
+    # ay * A + by * B = py  (equation 2)
+
+    # Multiply equation 1 by by and equation 2 by -bx to eliminate B
+    a_clicks_x = machine.button_a_x * machine.button_b_y
+    a_clicks_y = -(machine.button_a_y * machine.button_b_x)
+    prize_x = machine.prize_x * machine.button_b_y
+    prize_y = -(machine.prize_y * machine.button_b_x)
+
+    a_clicks_combined = a_clicks_x + a_clicks_y
+    prize_combined = prize_x + prize_y
+
+    # Check if we have an integer solution for A
+    return nil if prize_combined % a_clicks_combined != 0
+
+    a_clicks = prize_combined / a_clicks_combined
+    b_clicks = (machine.prize_x - (machine.button_a_x * a_clicks)) / machine.button_b_x
+
+    # Check if B is an integer and both A and B are non-negative
+    return nil if !b_clicks.is_a?(Integer) || a_clicks < 0 || b_clicks < 0
+
+    # Calculate total tokens needed (3 for A, 1 for B)
+    a_clicks * 3 + b_clicks
+  end
+end
+
+# day13.rb
 require_relative 'common'
 
 class Day13 < AdventDay
   def first_part
+    game = ClawGame.new(input)
+    game.minimum_tokens_needed
   end
 
   def second_part
